92e02f71a33f600d83a1f3d06d9667b3ac222ad0
Mohamed Rifat <mohamed.rifat100@gmail.com>
2026-01-30 07:49:55 +0200

    fix router: reset scroll position on route change

--- src/Component/Layout/Layout.jsx
+++ src/Component/Layout/Layout.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
 import { Outlet } from 'react-router-dom'
+import ScrollToTop from './../ScrollToTop/ScrollToTop'
 
 export default function Layout() {
     return (
         <>
+            <ScrollToTop />
             <Navbar />
             <Outlet />
             <Footer />
